312809be1175042a530b43ec8c094d8270cef6e8
Claudia <claudiaareneee@gmail.com>
2019-11-18 00:56:05 -0600

    fix for heroku stylesheet, might have to revert to test on localhost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(http);
 
 // var socket = io();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
